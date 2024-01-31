17bc874bd18b4004276d150ad08d08f5acef5c3d
Marc Duiker <marcduiker@users.noreply.github.com>
2024-01-31 22:37:01 +0100

    Upgrade the CLI

--- install-scripts/devcontainer-csharp.sh
+++ install-scripts/devcontainer-csharp.sh
@@ -1,6 +1,8 @@
+printf "\n\nHold on, we're installing some tools and building the project...\n\n"
 sudo apt-get update
 # xdg-utils is used to allow access to the default browser
 sudo apt-get install -y xdg-utils
 curl -o- https://downloads.diagrid.io/cli/install-catalyst.sh | bash
 sudo mv ./diagrid /usr/local/bin
+diagrid update --approve
 dotnet build ./csharp
